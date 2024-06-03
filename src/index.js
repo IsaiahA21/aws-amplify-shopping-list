27b2ba93142f9c9e0f568db1abaa8f56bafe8b67
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-06-03 16:50:27 -0600

    chore: Remove Amplify files and update npm dependencies

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
+// Configure Amplify
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+
+  <Authenticator>
     <App />
-  </React.StrictMode>
+  </Authenticator>
 );
 
 // If you want to start measuring performance in your app, pass a function
